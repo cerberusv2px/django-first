56ded6cb2507b0a7be56cd3308b3be111fb79c90
sujin <sujin.v2px@gmail.com>
2021-08-08 21:50:16 +0545

    login sample

--- djangoProject/urls.py
+++ djangoProject/urls.py
@@ -20,5 +20,6 @@ from register import views as v
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("main.urls")),
-    path('register/', v.register, name='register')
+    path('register/', v.register, name='register'),
+    path('', include('django.contrib.auth.urls')),
 ]
